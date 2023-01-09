2f878b64246892f208f06102a88edc8749071bd8
FJanko <fab.jankowi@gmail.com>
2023-01-09 12:26:08 +0100

    premier push

---
+++ src/main/java/fr/wf3/alphabetise/AlphabetiseApplication.java
@@ -0,0 +1,13 @@
+package fr.wf3.alphabetise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphabetiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphabetiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/wf3/alphabetise/AlphabetiseApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.wf3.alphabetise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphabetiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
