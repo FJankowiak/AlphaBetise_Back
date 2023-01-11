ea858ea48c258a52979d2014cf9efb10583ba95f
FJanko <fab.jankowi@gmail.com>
2023-01-11 11:22:51 +0100

    correction controller editeur, ajout d'une config spring simple

---
+++ src/main/java/fr/wf3/alphabetise/web/EditeurController.java
@@ -0,0 +1,26 @@
+package fr.wf3.alphabetise.web;
+
+//import org.springframework.stereotype.Controller;
+import fr.wf3.alphabetise.entities.Editeur;
+import fr.wf3.alphabetise.services.EditeurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/editeur")
+public class EditeurController {
+    @Autowired
+    private EditeurService editeurService;
+
+    @GetMapping
+    public ResponseEntity<List<Editeur>> getAllEditeurs(){
+        System.err.println("je suis là");
+        return new ResponseEntity<>(editeurService.findAllEditeur(), HttpStatus.OK);
+    }
+}
