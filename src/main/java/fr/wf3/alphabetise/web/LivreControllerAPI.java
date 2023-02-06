bb6d1bf14bf8d2ed06a29ece6c7c8d2bf1a6471d
Crissy31 <christine.andreck@gmail.com>
2023-02-06 16:25:29 +0100

    Mise à jour configuration

--- src/main/java/fr/wf3/alphabetise/web/LivreControllerAPI.java
+++ src/main/java/fr/wf3/alphabetise/web/LivreControllerAPI.java
@@ -1,16 +1,26 @@
 package fr.wf3.alphabetise.web;
 
+import fr.wf3.alphabetise.dtos.LivreDTO;
 import fr.wf3.alphabetise.services.LivreService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/livres")
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(value="/livres")
 @RestController
 public class LivreControllerAPI {
     private LivreService livreService;
 
     public LivreControllerAPI(LivreService livreService) {
         this.livreService = livreService;
+    }
 
+    @GetMapping("/books")
+    List<LivreDTO> findAllLivres(){
+        return this.livreService.booksList();
     }
 }
