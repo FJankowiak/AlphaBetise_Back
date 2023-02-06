0302825ab19f92d873a4f6063f43ab6d503ff51e
Andynator1 <andyjeanty249@gmail.com>
2023-02-06 16:58:20 +0100

    CRUD Livre version numero-1

--- src/main/java/fr/wf3/alphabetise/mappers/LivreMapperImpl.java
+++ src/main/java/fr/wf3/alphabetise/mappers/LivreMapperImpl.java
@@ -1,9 +1,22 @@
 package fr.wf3.alphabetise.mappers;
 
+import fr.wf3.alphabetise.dtos.LivreDTO;
+import fr.wf3.alphabetise.entities.Livre;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LivreMapperImpl {
+    public LivreDTO fromBook(Livre livre){
+        LivreDTO livreDTO=new LivreDTO();
+        BeanUtils.copyProperties(livre,livreDTO);
+        return  livreDTO;
+    }
+    public Livre fromBookDTO(LivreDTO livreDTO){
+        Livre livre=new Livre();
+        BeanUtils.copyProperties(livreDTO,livre);
+        return  livre;
+    }
 
 
 }
--- src/main/java/fr/wf3/alphabetise/web/LivreControllerAPI.java
+++ src/main/java/fr/wf3/alphabetise/web/LivreControllerAPI.java
@@ -1,16 +1,24 @@
 package fr.wf3.alphabetise.web;
 
+import fr.wf3.alphabetise.dtos.LivreDTO;
 import fr.wf3.alphabetise.services.LivreService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/livres")
+import java.util.List;
+
 @RestController
+@AllArgsConstructor
+//@CrossOrigin("*")
 public class LivreControllerAPI {
     private LivreService livreService;
 
-    public LivreControllerAPI(LivreService livreService) {
-        this.livreService = livreService;
 
+    @GetMapping("/Livres")
+    public List<LivreDTO> books(){
+        return livreService.booksList();
     }
 }
